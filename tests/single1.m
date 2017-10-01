69d9046cd97f482c951314b954ae5e5457cb206f
jucestain <jstain@gmail.com>
2017-09-30 23:27:13 -0500

    made calibration output a struct; still need to refactor stereo calibration

--- tests/single1.m
+++ tests/single1.m
@@ -15,96 +15,62 @@ cb_img_paths = {'images/Image1.tif', ...
 cb_imgs = class.img.validate_similar_imgs(cb_img_paths);
                      
 %% Load calibration config file
-cal_config = util.load_cal_config('configs/single.conf');
+calib_config = util.load_calib_config('configs/single.conf');
 
 %% Get four points in pixel coordinates per calibration board image
-four_points_ps = {};
-switch cal_config.calibration
-    case 'four_point_auto'
-        error('Automatic four point detection has not been implemented yet');
-    case 'four_point_manual'
-        % Four points are selected manually
-        [~, four_points_w] = alg.cb_points(cal_config);
+four_points_ps{1} = [168.7272  179.7808
+                     133.9287  377.5377
+                     413.8812  194.8252
+                     472.3155  386.7228];
 
-        four_points_ps{1} = [168 179;
-                             135 376;
-                             415 194;
-                             472 385];
-                       
-        four_points_ps{2} = [173 106;
-                             138 378;
-                             443 127;
-                             481 389];
-                      
-        four_points_ps{3} = [199 97;
-                             117 354;
-                             472 142;
-                             461 412]; 
-                     
-        four_points_ps{4} = [250 113;
-                             145 359;
-                             533 88;
-                             509 392]; 
-                     
-        four_points_ps{5} = [84 196;
-                             403 429;
-                             222 43;
-                             432 217]; 
-                    
-        four_points_ps{6} = [90 127;
-                             117 398;
-                             535 172;
-                             423 413]; 
-                    
-        four_points_ps{7} = [181 128;
-                             164 452;
-                             482 94;
-                             406 350]; 
-                     
-        four_points_ps{8} = [72 100;
-                             88 426;
-                             372 65;
-                             325 321]; 
-        % Refine
-        for i = 1:length(four_points_ps)
-            four_points_ps{i} = alg.refine_points(four_points_ps{i}, ...
-                                                  cb_imgs(i), ...
-                                                  alg.homography(four_points_w,four_points_ps{i},cal_config), ...
-                                                  cal_config); 
-        end   
-end
+four_points_ps{2} = [172.3350  108.0685
+                     138.0881  379.0046
+                     444.4066  129.1560
+                     481.5573  389.7946];
+
+four_points_ps{3} = [199.7681  98.1806
+                     117.8841  354.4820
+                     472.0018  142.9956
+                     460.8990  413.7783]; 
+
+four_points_ps{4} = [250.2379  114.2989
+                     145.3761  361.4282
+                     534.0855  89.3777
+                     509.4070  393.5712]; 
+
+four_points_ps{5} = [82.6358   195.8193
+                     403.7691  430.2562
+                     223.6562  44.6192
+                     432.8029  218.4991]; 
+
+four_points_ps{6} = [91.4560   129.4162
+                     117.4309  398.5697
+                     535.8790  172.1372
+                     423.0276  413.6498]; 
+
+four_points_ps{7} = [181.9291  128.9180
+                     165.0031  453.1418
+                     481.8723  96.0359
+                     406.0600  351.5853]; 
+
+four_points_ps{8} = [72.2896   102.1230
+                     88.7938   426.2946
+                     371.3808  66.0314
+                     325.0443  322.2007]; 
 
 %% Perform single calibration
-[A,distortion,rotations,translations,board_points_ps,homographies_refine] = alg.single_calibrate(cb_imgs, ...
-                                                                                                 four_points_ps, ...
-                                                                                                 cal_config);
+calib = alg.single_calib_four_points(cb_imgs, ...
+                                     four_points_ps, ...
+                                     calib_config);
 
 %% Save calibration
-util.write_single_calib(cb_imgs, ...
-                        board_points_ps, ...
-                        four_points_ps, ...
-                        A, ...
-                        distortion, ...
-                        rotations, ...
-                        translations, ...
-                        homographies_refine, ...
-                        cal_config, ...
-                        'calibrations/single1.txt');
+util.write_single_calib(calib,'calibrations/single1.txt');
                     
 %% Read calibration
 clear;
 
-[cb_imgs,board_points_ps,four_points_ps,A,distortion,rotations,translations,homographies_refine,cal_config] = util.read_single_calib('calibrations/single1.txt');
+calib = util.read_single_calib('calibrations/single1.txt');
 
 %% Debug with gui
 f = figure(1);
-debug.gui_single(cb_imgs, ...
-                 board_points_ps, ...
-                 four_points_ps, ...
-                 A, ...
-                 distortion, ...
-                 rotations, ...
-                 translations, ...
-                 homographies_refine, ...
-                 cal_config, ...
-                 f);
+debug.gui_single_calib(calib, f);

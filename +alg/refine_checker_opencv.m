05f5b0ed88ce9822475e2bbd378eb244afefc7c8
justinblaber <justin.akira.blaber@gmail.com>
2019-06-16 15:18:06 -0500

    minor update of refine_checker_opencv to add normalization step, from tests it does not seem to make a different though

--- +alg/refine_checker_opencv.m
+++ +alg/refine_checker_opencv.m
@@ -18,14 +18,34 @@ function [p, cov_p] = refine_checker_opencv(array_dx, array_dy, W)
     % Get coordinates of pixels
     bb_array = alg.bb_array(array_dx);
     [ys, xs] = alg.ndgrid_bb(bb_array);
-    xs = xs(:);
-    ys = ys(:);
+    ps = [xs(:) ys(:)];
+
+    % Perform normalization
+    T = norm_mat(ps);
+    p_augs = [ps ones(size(ps, 1), 1)]';
+    p_norms = T*p_augs;
 
     % Form linear system
     A = [array_dx(:) array_dy(:)];
-    b = array_dx(:).*xs + array_dy(:).*ys;
+    b = array_dx(:).*p_norms(1, :)' + array_dy(:).*p_norms(2, :)';
 
     % Solve
     [p, ~, ~, cov_p] = alg.safe_lscov(A, b, W(:));
-    p = p';
+
+    % "Undo" normalization
+    T_inv = alg.safe_inv(T);
+    p = T_inv*[p; 1];
+    p = p(1:2)';
+    cov_p = T_inv(1:2, 1:2)*cov_p*T_inv(1:2, 1:2)';
+end
+
+function T_norm = norm_mat(ps)
+    xs = ps(:, 1);
+    ys = ps(:, 2);
+    mean_x = mean(xs);
+    mean_y = mean(ys);
+    s_m = sqrt(2)*size(ps, 1)./sum(sqrt((xs-mean_x).^2+(ys-mean_y).^2));
+    T_norm = [s_m 0   -mean_x*s_m;
+              0   s_m -mean_y*s_m;
+              0   0    1];
 end
